d17798a15ac63c0fa7ee1cfbb388bc52d952c9d1
ThibCL <thibault.cabourg@gmail.com>
2019-02-03 14:50:16 +0100

    modification qui empêche dajouter deux fois la même langue et qui permet dafficher les langues connues

--- koala/index.js
+++ koala/index.js
@@ -15,11 +15,23 @@ const path = require('path')
 const app = new koa()
 const router = new koaRouter()
 
+//liste qui contient toutes les façons de dire bonjour dans les langues connues
 var list=[]
 list.push({
   langue : 'Inconnu',
   hello : 'Je ne connais pas cette langue'
   })
+
+//Fonction qui permet de regarder si une langue est déja dans la liste des langues connues 
+function dansListe (lister,lgs) {
+	var res = true
+	for( var k=0 ; k<lister.length ; k++){
+		if(lister[k].langue==lgs){
+			res=false;
+		}
+	}
+	return res;
+}
 
 render(app, {
   root: path.join(__dirname, 'views'),
@@ -29,6 +41,7 @@ render(app, {
   debug: true
 })
 
+//Methode get qui permet de dire bonjour dans la langue voulu
 router.get('koala','/hello', (ctx) => {
   let lg = ctx.request.query.langue
   let i =0
@@ -40,33 +53,38 @@ router.get('koala','/hello', (ctx) => {
     i++
   }
   return ctx.render('index', {
-    liste : list[s].hello
+    liste : list[s].hello,
+    connu : list
    
   })
 })
 
+//Méthode get pour récupérer la page d'ajout d'une langue
 router.get('koala','/formulaire', (ctx) => {
   return ctx.render('formulaire')
   })
 
+//Méthode get pour récupérer la page de suppression d'une langue
 router.get('koala','/suppression', (ctx) => {
   return ctx.render('suppression')
   })
 
-
+//Méthode post qui permet d'apprendre une nouvelle langue
 router.post('koala','/hello', koaBody(), (ctx) => {
-  list.push({
-  langue : ctx.request.body.langue,
-  hello : ctx.request.body.hello
-  })
-  console.log(ctx.request.body)
+  if(dansListe(list,ctx.request.body.langue)){
+	  list.push({
+	  langue : ctx.request.body.langue,
+	  hello : ctx.request.body.hello
+	  })
+  }
   return ctx.render('index', {
-    liste : list[0].hello
+    liste : list[0].hello,
+    connu : list
    
   })
 })
 
-
+//Méthode post qui permet de supprimer une langue
 router.post('koala','/sup', koaBody(), (ctx) => {
   let lg = ctx.request.body.langue
   console.log(lg)
@@ -78,7 +96,8 @@ router.post('koala','/sup', koaBody(), (ctx) => {
     j++
   }
   return ctx.render('index', {
-    liste : list[0].hello
+    liste : list[0].hello,
+    connu : list
    
   })
 })
@@ -88,9 +107,7 @@ app.use(router.routes())
 
 
 
-/*app.use(function*(){ 
-	this.body="hello";
-});*/
+
 
 
 
