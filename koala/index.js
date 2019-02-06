d3116a8870604351e60bbb22bf4e26ffd91c3076
ThibCL <thibault.cabourg@gmail.com>
2019-02-06 21:37:57 +0100

    Ajout d'un fichier logger pour gérer winston

--- koala/index.js
+++ koala/index.js
@@ -8,19 +8,11 @@ const Store = require("./store.js");
 const koa = require("koa");
 const koaBody = require("koa-body");
 const koaRouter = require("koa-router");
-const { createLogger, format, transports } = require("winston");
+const logger = require("./logger.js");
 
 //Const variables
 const app = new koa();
 const router = new koaRouter();
-
-const logger = createLogger({
-  level: "debug",
-  format: format.combine(format.colorize(), format.simple()),
-  // You can also comment out the line above and uncomment the line below for JSON format
-  // format: format.json(),
-  transports: [new transports.Console()]
-});
 
 //list which contains all the language know by the server
 let list = [];
@@ -29,7 +21,9 @@ let str = new Store();
 //Request get that give you the way to say hello in any language
 router.get("koala", "/hello", ctx => {
   let lg = ctx.request.query.langue;
+  logger.info(lg);
   let rep = str.getHello(lg);
+  logger.info(rep);
   ctx.response.status = rep[0];
   ctx.body = rep[1];
 
@@ -39,6 +33,7 @@ router.get("koala", "/hello", ctx => {
 //Request post to add new language
 router.post("koala", "/hello", koaBody(), ctx => {
   let rep = str.addHello(ctx.request.body.langue, ctx.request.body.hello);
+  logger.info(rep);
   ctx.response.status = rep[0];
   ctx.response.body = rep[1];
   return ctx;
@@ -47,7 +42,9 @@ router.post("koala", "/hello", koaBody(), ctx => {
 //Request delete to delete one of the language
 router.delete("koala", "/hello", ctx => {
   let lg = ctx.request.query.langue;
+  logger.info(lg);
   let rep = str.deleteHello(lg);
+  logger.info(rep);
   ctx.response.status = rep[0];
   ctx.response.body = rep[1];
   return ctx;
